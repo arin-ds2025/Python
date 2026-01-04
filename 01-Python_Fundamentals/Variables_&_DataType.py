5d3e2e484f6436a7fe42f61afaa6761c075cd363
Arin Ahmed <arinwebdev2024@gmail.com>
2026-01-04 12:30:38 +0600

    first commit

---
+++ 01-Python_Fundamentals/Variables_&_DataType.py
@@ -0,0 +1,35 @@
+age =  21  # integer data type
+name = "Arin Ahmed" # string data type
+cgpa = 3.61 # float data type
+is_married = False # bool data type
+
+print("name: ",name,", age: ",age,", cgpa: ",cgpa,", is married: ",is_married)
+print("name: ",type(name))
+print("age: ",type(age))
+print("cgpa: ",type(cgpa))
+print("is_married: ",type(is_married))
+
+
+
+'''
+    Rules of defining a variable in python:
+
+    # Variable names must start with a letter (a-z , A-Z) or an underscore (_).
+    # They can contain letters,numbers and underscores.
+    # Variable names are case-sensitive.
+    # Avoid using python keywords as variable names.
+'''
+
+
+'''
+    Python supports several built-in data types:
+
+    # Integers (int) : Whole numbers (-2,0,2)
+    # Floats (float) : Decimal numbers (3.14, -0.0235)
+    # Strings (str) : Text data enclosed in quotes
+    # Booleans (bool) : Represents True or False
+    # Lists : Ordered , mutable collections, Example: [1,2,3] 
+    # Tuples : Ordered , immutable collections, Example: (1,2,3)
+    # Sets : Unordered collections of unique elements, Example: {1,2,3} 
+    # Dictionaries : Key-Value pairs, Example: {"name":"Arin Ahmed" , "age":21 , "marital_status":False} 
+'''
